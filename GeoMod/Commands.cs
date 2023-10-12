a08a4d0b107fb8df9d4156327d3788af550b54b5
Евгений Коновалов <e.konovalov.blw@gmail.com>
2023-10-12 15:25:43 +0300

    Модуль реализован в минимально готовом виде

--- GeoMod/Commands.cs
+++ GeoMod/Commands.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using NetTopologySuite;
 using NetTopologySuite.Geometries;
 using NetTopologySuite.IO;
+using NtsBufferOps = NetTopologySuite.Operation.Buffer;
 using NetTopologySuite.Operation.Buffer;
 
 using Teigha.Runtime;
@@ -15,14 +16,23 @@ using Teigha.Geometry;
 using HostMgd.EditorInput;
 
 using Loader.CoreUtilities;
-
+using System.Text.RegularExpressions;
 
 namespace GeoMod
 {
     public class GeoCommands
     {
-        public static NtsGeometryServices GeometryServices;
+        internal static NtsGeometryServices GeometryServices;
         internal static double DefaultBufferDistance { get; set; } = 1d;
+
+        private static BufferParameters DefaultBufferParameters { get; } = new BufferParameters()
+        {
+            EndCapStyle = EndCapStyle.Round,
+            JoinStyle = NtsBufferOps.JoinStyle.Round,
+            QuadrantSegments = 6,
+            SimplifyFactor = 0.02d,
+            IsSingleSided = false
+        };
 
         static GeoCommands()
         {
@@ -32,9 +42,6 @@ namespace GeoMod
                                                                     new PrecisionModel(1000d),
                                                                     // default SRID
                                                                     -1,
-                                                                    /********************************************************************
-                                                                     * Note: the following arguments are only valid for NTS >= v2.2
-                                                                     ********************************************************************/
                                                                     // Geometry overlay operation function set to use (Legacy or NG)
                                                                     GeometryOverlay.NG,
                                                                     // Coordinate equality comparer to use (CoordinateEqualityComparer or PerOrdinateEqualityComparer)
@@ -56,12 +63,43 @@ namespace GeoMod
                 Entity?[] entities = psr.Value.GetObjectIds().Select(id => transaction.GetObject(id, OpenMode.ForRead) as Entity).ToArray();
                 Geometry?[] geometries = TransferGeometry(entities!, geometryFactory).ToArray();
 
-                WKTWriter writer = new WKTWriter()
+                WKTWriter writer = new()
                 {
                     OutputOrdinates = Ordinates.XY
                 };
-                string outputWkt = string.Join(",",geometries.Select(g => writer.Write(g)).ToArray());
+                string outputWkt = string.Join("\n", geometries.Select(g => writer.Write(g)).ToArray());
                 System.Windows.Forms.Clipboard.SetText(outputWkt);
+                transaction.Commit();
+            }
+        }
+
+        [CommandMethod("ИЗВКТ")]
+        public static void GeometryFromClipboardWkt()
+        {
+            Workstation.Define();
+            var geometryFactory = GeometryServices.CreateGeometryFactory();
+            string fromClipboard = System.Windows.Forms.Clipboard.GetText();
+            string[] matches = Regex.Matches(fromClipboard, @"[a-zA-Z]*\s?\(.*\)").Select(m => m.Value).ToArray();
+            WKTReader reader = new(GeometryServices);
+            Geometry[] geometries = matches.Select(m => reader.Read(m)).ToArray();
+            using (Transaction transaction = Workstation.TransactionManager.StartTransaction())
+            {
+                BlockTable? blockTable = transaction.GetObject(Workstation.Database.BlockTableId, OpenMode.ForRead) as BlockTable;
+                BlockTableRecord? modelSpace = transaction.GetObject(blockTable![BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+
+                List<Polyline> polylines = new List<Polyline>();
+                foreach (Geometry geom in geometries)
+                {
+                    polylines.AddRange(geom.ToDWGPolylines());
+                }
+                if (polylines.Count > 0)
+                {
+                    foreach (Polyline polyline in polylines)
+                    {
+                        modelSpace!.AppendEntity(polyline);
+                    }
+                }
+                transaction.Commit();
             }
         }
 
@@ -93,69 +131,72 @@ namespace GeoMod
                     return;
                 double bufferDistance = result.Value;
                 DefaultBufferDistance = bufferDistance;
-                Geometry[] buffers = geometries.Select(g => g!.Buffer(bufferDistance)).ToArray();
+                Geometry[] buffers = geometries.Select(g => g!.Buffer(bufferDistance, DefaultBufferParameters)).ToArray();
 
                 BlockTable? blockTable = transaction.GetObject(Workstation.Database.BlockTableId, OpenMode.ForRead) as BlockTable;
                 BlockTableRecord? modelSpace = transaction.GetObject(blockTable![BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
 
-                Polygon? union = geometryFactory.CreateGeometryCollection(buffers).Union() as Polygon;
-                LinearRing? exteriorRing = union!.ExteriorRing as LinearRing;
-                modelSpace!.AppendEntity(exteriorRing!.ToDWGPolyline());
-                foreach (LinearRing geom in union.InteriorRings)
+                Geometry union = geometryFactory.CreateGeometryCollection(buffers).Union();
+                foreach (Polyline pl in union.ToDWGPolylines())
                 {
-                    modelSpace.AppendEntity(geom.ToDWGPolyline());
+                    modelSpace!.AppendEntity(pl);
                 }
                 transaction.Commit();
             }
         }
 
-        [CommandMethod("NTSTEST", CommandFlags.UsePickSet)]
-        public static void Run()
+        [CommandMethod("ЗОНАДИФФ", CommandFlags.UsePickSet)]
+        public static void DiverseZone()
         {
             Workstation.Define();
             var geometryFactory = GeometryServices.CreateGeometryFactory();
-            string? wktText;
-            string? wktBuffer;
+
             using (Transaction transaction = Workstation.TransactionManager.StartTransaction())
             {
-                PromptEntityOptions peo = new("Выберите полилинию")
-                {
-                    AllowNone = false
-                };
-                peo.AddAllowedClass(typeof(Polyline), true);
-                PromptEntityResult result = Workstation.Editor.GetEntity(peo);
-                if (result.Status != PromptStatus.OK)
+                PromptSelectionResult psr = Workstation.Editor.GetSelection();
+                if (psr.Status != PromptStatus.OK)
                     return;
-                Polyline polyline = transaction.GetObject(result.ObjectId, OpenMode.ForRead) as Polyline;
-                LineString ntsGeometry = polyline.ToNTSGeometry(geometryFactory) as LineString;
+                Entity?[] entities = psr.Value.GetObjectIds().Select(id => transaction.GetObject(id, OpenMode.ForRead) as Entity).ToArray();
 
-                WKTWriter writer = new WKTWriter()
+                string[] layerNames = entities.Select(e => e!.Layer).Distinct().ToArray();
+                Dictionary<string, double> bufferSizes = new();
+                foreach (string layer in layerNames)
                 {
-                    OutputOrdinates = Ordinates.XY
-                };
-                wktText = writer.WriteFormatted(ntsGeometry);
-                BufferParameters parameters = new BufferParameters()
-                {
-                    EndCapStyle = EndCapStyle.Round,
-                    QuadrantSegments = 4,
-                    SimplifyFactor = 0.02d,
-                    JoinStyle = NetTopologySuite.Operation.Buffer.JoinStyle.Round
-                };
-                Polygon? buffer = ntsGeometry!.Buffer(2d, parameters) as Polygon;
-                Polyline bufferPolyline = buffer!.ToDWGPolyline();
+                    PromptDoubleOptions pdo = new($"Введите размер буферной зоны для слоя {layer}")
+                    {
+                        AllowNegative = false,
+                        AllowZero = true,
+                        AllowNone = false,
+                    };
+                    PromptDoubleResult result = Workstation.Editor.GetDouble(pdo);
+                    if (result.Status == PromptStatus.Cancel)
+                        return;
+                    if (result.Status != PromptStatus.OK || result.Value == 0)
+                        continue;
+                    bufferSizes[layer] = result.Value;
+                }
+
+                var buffers = (from Entity entity in entities
+                               where entity is Polyline
+                               let pl = entity as Polyline
+                               let buffer = pl.ToNTSGeometry(geometryFactory).Buffer(bufferSizes[pl.Layer], DefaultBufferParameters) as Polygon
+                               select buffer).ToArray();
+                if (buffers.Length == 0)
+                    return;
+                Geometry union = geometryFactory.CreateGeometryCollection(buffers).Union();
 
                 BlockTable? blockTable = transaction.GetObject(Workstation.Database.BlockTableId, OpenMode.ForRead) as BlockTable;
                 BlockTableRecord? modelSpace = transaction.GetObject(blockTable![BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
-                modelSpace!.AppendEntity(bufferPolyline);
 
-                wktBuffer = writer.WriteFormatted(buffer);
+                foreach (Polyline pl in union.ToDWGPolylines())
+                {
+                    modelSpace!.AppendEntity(pl);
+                }
                 transaction.Commit();
             }
-            Workstation.Editor.WriteMessage($"WKT текст выбранной полилинии:\n{wktText}\n");
-            Workstation.Editor.WriteMessage($"WKT текст буфера:\n{wktBuffer}\n");
         }
 
-        [CommandMethod("ZONEJOIN", CommandFlags.UsePickSet)]
+        [CommandMethod("ЗОНОБЪЕД", CommandFlags.UsePickSet)]
         public static void ZoneJoin()
         {
             Workstation.Define();
@@ -166,8 +207,8 @@ namespace GeoMod
                 PromptSelectionResult psr = Workstation.Editor.GetSelection();
                 if (psr.Status != PromptStatus.OK)
                     return;
-                ObjectId[] entities = psr.Value.GetObjectIds();
-                var closedPolylines = (from ObjectId id in entities
+                ObjectId[] entitiesIds = psr.Value.GetObjectIds();
+                var closedPolylines = (from ObjectId id in entitiesIds
                                        let entity = transaction.GetObject(id, OpenMode.ForWrite) as Entity
                                        where entity is Polyline pl && pl.Closed
                                        select entity as Polyline).ToArray();
@@ -177,23 +218,21 @@ namespace GeoMod
                     return;
                 }
 
-                if (closedPolylines.Length < entities.Length)
-                    Workstation.Editor.WriteMessage($"Не обработано {entities.Length - closedPolylines.Length} объектов, не являющихся замкнутыми полилиниями");
+                if (closedPolylines.Length < entitiesIds.Length)
+                    Workstation.Editor.WriteMessage($"Не обработано {entitiesIds.Length - closedPolylines.Length} объектов, не являющихся замкнутыми полилиниями");
 
                 Polygon?[] polygons = closedPolylines.Select(pl => pl.ToNTSGeometry(geometryFactory) as Polygon).ToArray();
-                polygons.Select(g => g.IsValid ? g : NetTopologySuite.Geometries.Utilities.GeometryFixer.Fix(g)).ToArray();
+                Geometry?[] fixedPolygons = polygons.Select(g => g!.IsValid ? g : NetTopologySuite.Geometries.Utilities.GeometryFixer.Fix(g)).ToArray();
 
                 BlockTable? blockTable = transaction.GetObject(Workstation.Database.BlockTableId, OpenMode.ForRead) as BlockTable;
                 BlockTableRecord? modelSpace = transaction.GetObject(blockTable![BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
 
                 foreach (Polyline pl in closedPolylines)
                     pl.Erase();
-                Polygon? union = geometryFactory.CreateGeometryCollection(polygons).Union() as Polygon;
-                LinearRing? exteriorRing = union!.ExteriorRing as LinearRing;
-                modelSpace!.AppendEntity(exteriorRing.ToDWGPolyline());
-                foreach (LinearRing geom in union.InteriorRings)
+                Geometry union = geometryFactory.CreateGeometryCollection(fixedPolygons).Union();
+                foreach (Polyline pl in union.ToDWGPolylines())
                 {
-                    modelSpace.AppendEntity(geom.ToDWGPolyline());
+                    modelSpace!.AppendEntity(pl);
                 }
 
                 transaction.Commit();
@@ -214,7 +253,7 @@ namespace GeoMod
                 }
                 else if (entity is BlockReference bref)
                 {
-                    geometries.Append(bref.ToNTSGeometry(geometryFactory));
+                    geometries.Add(bref.ToNTSGeometry(geometryFactory));
                 }
                 else
                 {
@@ -225,10 +264,23 @@ namespace GeoMod
                 Workstation.Editor.WriteMessage("Внимание! Кривые не поддерживаются. Для корректного вывода аппроксимируйте геометрию с дуговыми сегментами");
             return geometries;
         }
+
+        internal static Geometry? TransferGeometry(Entity entity, GeometryFactory geometryFactory)
+        {
+            if (entity is Polyline pl)
+            {
+                return pl.ToNTSGeometry(geometryFactory);
+            }
+            else if (entity is BlockReference bref)
+            {
+                return bref.ToNTSGeometry(geometryFactory);
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
-
-
-
 
     // Методы расширения для геометрий (и DWG, и NetTopology)
 
@@ -244,7 +296,16 @@ namespace GeoMod
             }
             if (polyline.Closed)
             {
-                return geometryFactory.CreatePolygon(coordinates);
+                if (coordinates[0].Equals(coordinates[coordinates.Length - 1]))
+                {
+                    return geometryFactory.CreatePolygon(coordinates);
+                }
+                else
+                {
+                    List<Coordinate> coordsClosed = new(coordinates);
+                    coordsClosed.Add(coordsClosed[0].Copy());
+                    return geometryFactory.CreatePolygon(coordsClosed.ToArray());
+                }
             }
             else
             {
@@ -262,7 +323,6 @@ namespace GeoMod
         }
     }
 
-
     public static class LineStringExtension
     {
         public static Polyline ToDWGPolyline(this LineString lineString)
@@ -272,7 +332,10 @@ namespace GeoMod
 
         internal static Polyline ProcessGeometry(Geometry geometry)
         {
-            Polyline polyline = new();
+            Polyline polyline = new()
+            {
+                LayerId = Workstation.Database.Clayer
+            };
             Coordinate[] coordinates = geometry.Coordinates;
             for (int i = 0; i < coordinates.Length; i++)
             {
@@ -292,19 +355,70 @@ namespace GeoMod
             return polyline;
         }
 
-        public static Polyline ToDWGPolyline(this Polygon polygon)
+        public static IEnumerable<Polyline> ToDWGPolylines(this Polygon polygon)
         {
-            Polyline polyline = LineStringExtension.ProcessGeometry(polygon);
-            polyline.Closed = true;
-            return polyline;
+            List<Polyline> polylines = new();
+            LinearRing? exteriorRing = polygon.ExteriorRing as LinearRing;
+            polylines.Add(exteriorRing!.ToDWGPolyline());
+            foreach (LinearRing geom in polygon.InteriorRings.Cast<LinearRing>())
+            {
+                polylines.Add(geom.ToDWGPolyline());
+            }
+            return polylines;
+        }
+
+        public static IEnumerable<Polyline> ToDWGPolylines(this MultiPolygon mPolygon)
+        {
+            List<Polyline> polylines = new();
+
+            foreach (Polygon? polygon in mPolygon.Geometries.Select(g => g as Polygon))
+            {
+                polylines.AddRange(polygon!.ToDWGPolylines());
+            }
+            return polylines;
+        }
+        public static IEnumerable<Polyline> ToDWGPolylines(this MultiLineString mLinestring)
+        {
+            List<Polyline> polylines = new();
+
+            foreach (LineString? linestring in mLinestring.Geometries.Select(g => g as LineString))
+            {
+                polylines.AddRange(linestring!.ToDWGPolylines());
+            }
+            return polylines;
+        }
+
+        public static IEnumerable<Polyline> ToDWGPolylines(this Geometry geometry)
+        {
+            Type type = geometry.GetType();
+            List<Polyline> polylines = new();
+
+            switch (type.Name.ToUpper())
+            {
+                case "LINESTRING":
+                    LineString? ls = geometry as LineString;
+                    polylines.Add(ls!.ToDWGPolyline());
+                    break;
+                case "LINEARRING":
+                    LinearRing? lr = geometry as LinearRing;
+                    polylines.Add(lr!.ToDWGPolyline());
+                    break;
+                case "POLYGON":
+                    Polygon? pg = geometry! as Polygon;
+                    polylines.AddRange(pg!.ToDWGPolylines());
+                    break;
+                case "MULTIPOLYGON":
+                    MultiPolygon? mpg = geometry! as MultiPolygon;
+                    polylines.AddRange(mpg!.ToDWGPolylines());
+                    break;
+                case "MULTILINESTRING":
+                    MultiLineString? mls = geometry! as MultiLineString;
+                    polylines.AddRange(mls!.ToDWGPolylines());
+                    break;
+                default:
+                    break;
+            }
+            return polylines;
         }
     }
-
-
-
-
-
-
-
-
 }
