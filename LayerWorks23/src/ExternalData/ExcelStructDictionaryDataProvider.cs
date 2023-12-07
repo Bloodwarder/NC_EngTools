9044ca93f200835cddca5b45a808e90008fdb606
Евгений Коновалов <e.konovalov.blw@gmail.com>
2023-12-07 18:26:32 +0300

    Починено взаимодействие с Excel. Добавлен класс параметров сетки условных

--- LayerWorks23/src/ExternalData/ExcelStructDictionaryDataProvider.cs
+++ LayerWorks23/src/ExternalData/ExcelStructDictionaryDataProvider.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+﻿using System.Reflection;
 using Microsoft.Office.Interop.Excel;
 using Excel = Microsoft.Office.Interop.Excel;
 
