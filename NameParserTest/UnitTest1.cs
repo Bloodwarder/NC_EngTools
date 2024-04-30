e5f6068916f3b4f9673532dd86ac3b753c0da2e4
Евгений <e.konovalov.blw@gmail.com>
2024-04-30 23:58:53 +0300

    Дописано несколько тестов. Парсер их проходит.

--- NameParserTest/UnitTest1.cs
+++ NameParserTest/UnitTest1.cs
@@ -3,27 +3,73 @@ using System.Xml.Linq;
 
 namespace NameParserTest
 {
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
+        XDocument? _xDocument;
+        LayerInfo? _layerInfo;
+        NameParser? _parser;
+
+        [OneTimeSetUp]
         public void Setup()
         {
             FileInfo fi = new FileInfo(Assembly.GetExecutingAssembly().Location);
-            XDocument xDocument = XDocument.Load(Path.Combine(fi.Directory!.FullName, "TestData", "LayerParserTemplate.xml"));
-            _ = new NameParser(xDocument);
+            _xDocument = XDocument.Load(Path.Combine(fi.Directory!.FullName, "TestData", "LayerParserTemplate.xml"));
+            _parser = new NameParser(_xDocument);
+        }
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _layerInfo = null;
+            _xDocument = null;
         }
         [Test]
-        public void Test1()
+        public void ParserInitializationTestWhenProper()
         {
-            NameParser? parser = NameParser.LoadedParsers.Values.First();
+            Assert.That(_parser, Is.Not.Null);
+            Assert.That(_parser.Prefix, Is.EqualTo("ИС"));
+        }
+        
+        [Test]
+        public void LayerInfoTestWhenProper()
+        {
+            _layerInfo = _parser!.GetLayerInfo("ИС_ЭС_л_КЛ_0.4кВ_пр");
+            Assert.That(_layerInfo, Is.Not.Null);
+            _layerInfo.ChangeAuxilaryData("ExternalProject", "ВСМ-2017");
+            Assert.That(_layerInfo.Name, Is.EqualTo("ИС_[ВСМ-2017]_ЭС_л_КЛ_0.4кВ_неутв"));
+            _layerInfo.ChangeAuxilaryData("ExternalProject", null);
+            Assert.That(_layerInfo.Name, Is.EqualTo("ИС_ЭС_л_КЛ_0.4кВ_неутв"));
+            _layerInfo.SuffixTagged = true;
+            Assert.That(_layerInfo.Name, Is.EqualTo("ИС_ЭС_л_КЛ_0.4кВ_неутв_пер"));
+        }
+        [Test]
+        public void LayerInfoWhenWrongShouldThrow()
+        {
+            try
+            {
+                _ = _parser.GetLayerInfo("вап_ыык44_аклвю341");
+            }
+            catch (Exception ex) 
+            {
+                Assert.That(ex, Is.InstanceOf<WrongLayerException>());
+                return;
+            }
+            Assert.Fail("Не выброшено исключение");
+        }
 
-            Assert.That(parser, Is.Not.Null);
-            Assert.That(parser.Prefix, Is.EqualTo("ИС"));
-
-            LayerInfo layerInfo = parser.GetLayerInfo("ИС_ЭС_л_КЛ_0.4кВ_пр");
-            Assert.That(layerInfo, Is.Not.Null);
-
-            Assert.Pass();
+        [Test]
+        public void LayerInfoWhenProperPrefixWrongInfoShouldThrow()
+        {
+            try
+            {
+                _ = _parser.GetLayerInfo("ИС_какая-то_рандомная_[хрень]_пер");
+            }
+            catch (Exception ex)
+            {
+                Assert.That(ex, Is.InstanceOf<WrongLayerException>());
+                return;
+            }
+            Assert.Fail("Не выброшено исключение");
         }
     }
 }
