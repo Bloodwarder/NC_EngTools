851f3e35923ffc0f851e09113c03d7bea1b93f01
Евгений Коновалов <e.konovalov.blw@gmail.com>
2024-05-16 10:18:55 +0300

    Доработана сериализация общих свойств

---
+++ NameParser/src/SharedProperties/SharedProperties.cs
@@ -0,0 +1,16 @@
+﻿using NameClassifiers.Filters;
+using System.Collections;
+using System.Xml.Serialization;
+
+
+namespace NameClassifiers.SharedProperties
+{
+    [XmlRoot("SharedProperties")]
+    public class SharedProperties //: ICollection<SharedProperty>
+    {
+        public SharedProperties() { }
+
+        [XmlElement(ElementName = "Property", Type = typeof(SharedProperty))]
+        public SharedProperty[] Properties { get; set; }
+    }
+}
---
+++ NameParser/src/SharedProperties/ValueContainer.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+
+namespace NameClassifiers.SharedProperties
+{
+    public class ValueContainer
+    {
+        public ValueContainer() { }
+        [XmlElement(ElementName = "Double", Type = typeof(double))]
+        [XmlElement(ElementName = "Color", Type = typeof(Color))]
+        [XmlElement(ElementName = "String", Type = typeof(string))]
+        public object? Value;
+    }
+}
