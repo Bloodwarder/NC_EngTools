2ff709eda11a3b921aecaf034a375174f96d6a76
Евгений Коновалов <e.konovalov.blw@gmail.com>
2024-12-04 18:23:39 +0300

    Добавлены миграции (создана начальная)

--- LayerDbMigrations/DesignTimeLayerDatabaseContextFactory.cs
+++ LayerDbMigrations/DesignTimeLayerDatabaseContextFactory.cs
@@ -4,7 +4,7 @@ namespace LayersIO.Connection
 {
     public class DesignTimeLayerDatabaseContextFactory : IDesignTimeDbContextFactory<LayersDatabaseContextSqlite>
     {
-        private const string SourceDatabasePath = @".\Data\LayerData_Testing.db";
+        private const string SourceDatabasePath = @".\Data\LayerData_Testing1.db";
         public LayersDatabaseContextSqlite CreateDbContext(string[] args)
         {
             return new(SourceDatabasePath, null);
