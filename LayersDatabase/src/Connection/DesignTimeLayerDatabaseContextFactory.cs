be66aa5f1f134d52b41b6e8940e299775aee3681
Евгений Коновалов <e.konovalov.blw@gmail.com>
2024-12-04 14:12:08 +0300

    Подготовка к реализации миграций

---
+++ LayersDatabase/src/Connection/DesignTimeLayerDatabaseContextFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+
+namespace LayersIO.Connection
+{
+    public class DesignTimeLayerDatabaseContextFactory : IDesignTimeDbContextFactory<LayersDatabaseContextSqlite>
+    {
+        private const string SourceDatabasePath = @"C:\Users\konovalove\source\repos\Bloodwarder\NC_EngTools\LayersDatabase\Data\LayerData_Testing.db";
+        public LayersDatabaseContextSqlite CreateDbContext(string[] args)
+        {
+            return new(SourceDatabasePath, null);
+        }
+    }
+}
