38fb1d89a1625156d4bf6cb182d57d32832d64a3
Евгений Коновалов <e.konovalov.blw@gmail.com>
2024-12-13 14:31:28 +0300

    ActiveLayerWrappers перенесён в статику LayerWrapper.ActiveWrappers

--- LayersDatabase/src/Database/Readers/SQLiteZoneInfoProvider.cs
+++ LayersDatabase/src/Database/Readers/SQLiteZoneInfoProvider.cs
@@ -14,11 +14,11 @@ namespace LayersIO.Database.Readers
         {
             using (var db = _contextFactory.CreateDbContext(_path))
             {
-                var layers = db.Layers.Include(l => l.Zones)
-                                      .ThenInclude(z => z.ZoneLayer)
-                                      .AsNoTracking()
+                var layers = db.Layers.AsNoTracking()
                                       .Where(l => l.Zones != null && l.Zones.Any())
                                       .Where(l => !string.IsNullOrEmpty(l.MainName) && !string.IsNullOrEmpty(l.StatusName))
+                                      .Include(l => l.Zones)
+                                      .ThenInclude(z => z.ZoneLayer)
                                       .AsEnumerable();
 
                 var kvpCollection = layers.Select(l => new KeyValuePair<string, ZoneInfo[]>
@@ -31,8 +31,12 @@ namespace LayersIO.Database.Readers
         {
             using (var db = _contextFactory.CreateDbContext(_path))
             {
-                var layer = db.Layers.Where(l => l.Name == key).Include(l => l.Zones).Single();
-                var zones = layer.Zones.Select(z => z.ToZoneInfo()).ToArray();
+                var layer = db.Layers.AsNoTracking()
+                                     .Where(l => l.Name == key && l.Zones.Any())
+                                     .Include(l => l.Zones)
+                                     .ThenInclude(z => z.ZoneLayer)
+                                     .SingleOrDefault();
+                var zones = layer?.Zones.Select(z => z.ToZoneInfo()).ToArray();
                 return zones;
             }
         }
