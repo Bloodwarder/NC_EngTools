e93a2d9139e2a6102b0eef6cb2976e83b8ca7304
Евгений Коновалов <e.konovalov.blw@gmail.com>
2025-02-04 15:09:03 +0300

    Команды изменения слоёв меньше зависят от текущего парсера

---
+++ LoaderCore/src/UI/ErrorListWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using LoaderCore.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LoaderCore.UI
+{
+    /// <summary>
+    /// Логика взаимодействия для ErrorListWindow.xaml
+    /// </summary>
+    public partial class ErrorListWindow : Window
+    {
+        public ErrorListWindow(IEnumerable<ErrorEntry> entries, string listName)
+        {
+            InitializeComponent();
+            this.Title = listName;
+            foreach (var entry in entries)
+            {
+                Errors.Add(entry);
+            }
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+        public ObservableCollection<ErrorEntry> Errors { get; } = new();
+
+        private void errorListWindow_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            var focusedElement = FocusManager.GetFocusedElement(this);
+            if ((e.Key == Key.Enter || e.Key == Key.Escape) && focusedElement is not CheckBox)
+            {
+                e.Handled = true;
+                this.Close();
+            }
+        }
+
+        private void errorListWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.PreviewKeyDown += errorListWindow_PreviewKeyDown;
+        }
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var scrollViewer = sender as ScrollViewer;
+            if (scrollViewer != null)
+            {
+                scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - e.Delta);
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ LoaderCore/src/Utilities/ErrorEntry.cs
@@ -0,0 +1,13 @@
+﻿namespace LoaderCore.Utilities
+{
+    public record ErrorEntry
+    {
+        public ErrorEntry(string elementName, string message)
+        {
+            ElementName = elementName;
+            Message = message;
+        }
+        public string ElementName { get; }
+        public string Message { get; }
+    }
+}
